8e1b7bedf301ca8fc4c320f88f8d3822707a7c4a
若海 <wang@rehiy.com>
2023-10-27 12:13:52 +0800

    feat: k8s安装grafpro自动写入配置

--- grafpro/kubernetes.sh
+++ grafpro/kubernetes.sh
@@ -5,23 +5,6 @@
 
 . ./config
 
-#### 创建配置 ###
-
-mkdir -p $GRAFPRO_STORAGE/etc
-
-cat <<EOF >$GRAFPRO_STORAGE/etc/prometheus.yml
-global:
-  scrape_interval: 5s
-  scrape_timeout: 5s
-  evaluation_interval: 15s
-
-scrape_configs:
-  - job_name: prometheus
-    static_configs:
-      - targets:
-          - 127.0.0.1:9090
-EOF
-
 #### 安装应用 ####
 
 cat kubernetes.yaml \
